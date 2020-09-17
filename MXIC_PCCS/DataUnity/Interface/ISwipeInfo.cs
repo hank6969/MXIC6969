37c499d867e5b1e611a9a9185ffbaa569e2a40d9
HankYang <HankYang@HANKYANG-NB>
2020-09-17 11:53:33 +0800

    test
    test

---
+++ MXIC_PCCS/DataUnity/Interface/ISwipeInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MXIC_PCCS.DataUnity.Interface
+{
+    interface ISwipeInfo
+    {
+        string CheckinList(string VendorName, string EmpID, string EmpName ,DateTime? StartTime, DateTime? EndTime, string AttendTypeSelect);
+
+        string SwipeInfoDetail(string EditID);
+
+        string EditSwipe(string EditID, string AttendTypeSelect, string Hour);
+
+         string transform();
+    }
+}

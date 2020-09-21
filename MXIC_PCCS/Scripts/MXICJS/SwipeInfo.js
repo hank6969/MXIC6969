71ed3c413299297e3de3ca5c4c68f9e4e1d2ea5e
jamesx0517 <jamesx0517@gmail.com>
2020-09-21 17:47:17 +0800

    畫面個別下拉式選單製作

---
+++ MXIC_PCCS/Scripts/MXICJS/SwipeInfo.js
@@ -0,0 +1,10 @@
+var dataList = [
+    { 'name': '正常', 'value': '正常' },
+    { 'name': '異常', 'value': '異常' },
+    { 'name': '加班', 'value': '加班' },
+    { 'name': '遲到', 'value': '遲到' },
+    { 'name': '早退', 'value': '早退' },
+    { 'name': '代早', 'value': '代早' },
+    { 'name': '代晚', 'value': '代晚' },
+
+]
